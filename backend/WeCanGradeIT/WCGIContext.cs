feb8209c0d1ee477cad996e933b14242e8f83e1c
Joe Scheiman <scheiman.joe@gmail.com>
2019-04-02 12:06:03 -0400

    Added Seed Data

--- backend/WeCanGradeIT/WCGIContext.cs
+++ backend/WeCanGradeIT/WCGIContext.cs
@@ -21,5 +21,30 @@ namespace WeCanGradeIT
 
             base.OnConfiguring(optionsBuilder);
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Assignment>().HasData(
+                new Assignment()
+                {
+                    Name = "University Clinic Hospital",
+                    Type = "Individual",
+                    Description = "Create a set of classes that contain information on employees at University Clinic Hospital.",
+                    Requirements = new List<string>()
+                        { "Base Class", "Derived Class", "Abstract Class"},
+                    DueDate = new DateTime(2019, 4, 24, 9, 30, 0)
+                },
+                new Assignment()
+                {
+                    Name = "Professional Portfolio",
+                    Type = "Individual",
+                    Description = "Create a portfolio site highlighting your work that can be shared with potential employers.",
+                    Requirements = new List<string>()
+                        { "HTML", "CSS", "CSS Grid or Flexbox" },
+                    DueDate = new DateTime(2019, 3, 15, 9, 30, 0)
+                }
+
+            );
+        }
     }
 }
