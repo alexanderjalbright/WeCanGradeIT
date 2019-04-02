27a6cb8a6692e895f12fe9841d42a3f11510f3a4
Alex Albright <alexanderjalbright@gmail.com>
2019-04-02 15:38:04 -0400

    Added routing for assignment components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,42 @@
+import { BrowserRouter as Router, Link, Route } from "react-router-dom";
+
 import React, { Component } from "react";
 import Assignment from "./components/Assignment";
 import "./App.css";
-
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       assignments: []
-    }
+    };
   }
   componentDidMount() {
     fetch("https://localhost:44397/api/assignment")
-    .then(res => res.json())
-    .then(json => this.setState({ assignments: json}));
+      .then(res => res.json())
+      .then(json => this.setState({ assignments: json }));
   }
 
   render() {
     const parseAssignments = this.state.assignments.map(assignment => (
-      <Assignment key={assignment.assignmentId} 
-        assignment={assignment}/>
-    ) )
+      <Route
+        path={`/assignment/${assignment.assignmentId}`}
+        exact={true}
+        component={() => (
+          <Assignment key={assignment.assignmentId} assignment={assignment} />
+        )}
+      />
+    ));
+    const assignLinks = this.state.assignments.map(assignment => (
+      <Link to={`/assignment/${assignment.assignmentId}`}>
+        {assignment.name}
+      </Link>
+    ));
     return (
-      <div className="App">
-        {parseAssignments}
-      </div>
-      
+      <Router>
+        <nav>{assignLinks}</nav>
+        <div className="App">{parseAssignments}</div>
+      </Router>
     );
   }
 }
