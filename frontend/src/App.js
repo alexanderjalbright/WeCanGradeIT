8b6bd9f0de0cf1e986e9938d1a4cbfba4ae5e221
Joe Scheiman <scheiman.joe@gmail.com>
2019-04-02 14:21:12 -0400

    Created first component; Assignment

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,31 @@
 import React, { Component } from "react";
+import Assignment from "./components/Assignment";
 import "./App.css";
 
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      assignments: []
+    }
+  }
+  componentDidMount() {
+    fetch("https://localhost:44397/api/assignment")
+    .then(res => res.json())
+    .then(json => this.setState({ assignments: json}));
+  }
+
   render() {
+    const parseAssignments = this.state.assignments.map(assignment => (
+      <Assignment key={assignment.assignmentId} 
+        assignment={assignment}/>
+    ) )
     return (
       <div className="App">
-        <p>Placeholder</p>
+        <p></p>
       </div>
+      
     );
   }
 }
---
+++ frontend/src/components/Assignment.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import "./App.css";
+
+export default class Assignment extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
