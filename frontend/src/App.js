ae6ec48f191988446eb8f2500a03def134338a52
Alex Albright <alexanderjalbright@gmail.com>
2019-04-03 14:58:38 -0400

    Added selector for user on home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,13 +8,22 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      assignments: []
+      assignments: [],
+      students: [{ name: "Instructor", userName: "Instructor", studentId: 0 }],
+      user: {}
     };
   }
+
   componentDidMount() {
     fetch("https://localhost:44397/api/assignment")
       .then(res => res.json())
       .then(json => this.setState({ assignments: json }));
+    fetch("https://localhost:44397/api/student")
+      .then(res => res.json())
+      .then(json => {
+        json.unshift(this.state.students[0]);
+        this.setState({ students: json });
+      });
   }
 
   render() {
@@ -27,11 +36,30 @@ class App extends Component {
         )}
       />
     ));
+
+    const parseStudents = this.state.students.map(student => (
+      <Route
+        path={`/student/${student.studentId}`}
+        exact={true}
+        component={() => <h1>Hi, {this.state.user.name}</h1>}
+      />
+    ));
+
     const assignLinks = this.state.assignments.map(assignment => (
       <Link to={`/assignment/${assignment.assignmentId}`}>
         {assignment.name}
       </Link>
     ));
+
+    const selectUserLinks = this.state.students.map(student => (
+      <option
+        value={student.name}
+        onClick={() => this.setState({ user: student })}
+      >
+        {student.name}
+      </option>
+    ));
+
     return (
       <Router>
         <nav>
@@ -45,7 +73,29 @@ class App extends Component {
           </h1>
           {assignLinks}
         </nav>
-        <div className="App">{parseAssignments}</div>
+        <div className="App">
+          <Route
+            path={`/`}
+            exact={true}
+            component={() => (
+              <div>
+                <select>{selectUserLinks}</select>
+                <Link
+                  to={`/${
+                    this.state.user.name === "Instructor"
+                      ? `instructor`
+                      : `${this.state.user.userName}`
+                  }`}
+                >
+                  <button>Enter</button>
+                </Link>
+                <h2>{this.state.user.name} has been selected!</h2>
+              </div>
+            )}
+          />
+          {parseAssignments}
+          {parseStudents}
+        </div>
       </Router>
     );
   }
