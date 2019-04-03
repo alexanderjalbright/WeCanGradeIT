445c0bf9755b083b661b5db0eb03e5c2df5ac8a0
Joe Scheiman <scheiman.joe@gmail.com>
2019-04-03 14:00:52 -0400

    Created Function to Parse MD List to Array

--- frontend/src/components/Assignment.js
+++ frontend/src/components/Assignment.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
+import parseMarkdown from '../parseMarkdown';
 
 export default class Assignment extends Component {
   render() {
-      const {assignmentId, name, type, description, requirements, dueDate} = this.props.assignment;
+    const {assignmentId, name, type, description, requirements, dueDate} = this.props.assignment;
+
+    const requirementsArray = parseMarkdown(requirements);
+
+    const renderArrayToHTMLList = requirementsArray.map(req => <li key={req}>{req}</li>);
+
     return (
       <div>
         <h1>{name}</h1>
         <p>{description}</p>
         <h2>{type}</h2>
         <h2>Due: {dueDate}</h2>
-        <h2>{requirements}</h2>
+        <ul>{renderArrayToHTMLList}</ul>
       </div>
     )
   }
