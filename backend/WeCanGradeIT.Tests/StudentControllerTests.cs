b34d513f6d62bd03b64c7a0364c76c9bc166df27
Zach Dickens <dickens.52@buckeyemail.osu.edu>
2019-04-03 10:43:09 -0400

    Added StudentController Model and Tests

---
+++ backend/WeCanGradeIT.Tests/StudentControllerTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace WeCanGradeIT.Tests
+{
+    public class StudentControllerTests
+    {
+        underTest;
+        public StudentControllerTests()
+        {
+            
+        }
+
+        [Fact]
+        public void Get_Returns_A_Student()
+        {
+
+        }
+    }
+}
---
+++ backend/WeCanGradeIT/Models/Student.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WeCanGradeIT.Models
+{
+    public class Student
+    {
+    }
+}
