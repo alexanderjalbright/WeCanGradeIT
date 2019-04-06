2eed3f0bca7571abe2716b158d0a60a2f2a81090
Alex Albright <alexanderjalbright@gmail.com>
2019-04-06 11:46:44 -0400

    assignments are now a dropdown

--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -6,44 +6,56 @@ export default class Nav extends Component {
   render() {
     const { user, assignments } = this.props;
     const assignLinks = assignments.map(assignment => (
-      <Route
-        key={assignments.assignmentId}
-        path={user.userName === "" ? "" : `/${user.userName}/assignments`}
-        component={() => (
-          <Link to={`/${user.userName}/assignments/${assignment.assignmentId}`}>
-            {assignment.name}
-          </Link>
-        )}
-      />
+      <Link to={`/${user.userName}/assignments/${assignment.assignmentId}`}>
+        <li>{assignment.name}</li>
+      </Link>
     ));
     return (
       <nav>
-        <h1>
-          <span className="we">We</span>
-          <span className="can">Can</span>
-          <span className="grade">
-            {`{`}Grade{`}`}
-          </span>
-          <span className="it">IT</span>
-        </h1>
-        <Route
-          path={`/${user.userName}`}
-          component={() => (
-            <Link to={`/${user.userName}/assignments`}>Assignments</Link>
-          )}
-        />
-        <Route
-          path={`/${user.userName}`}
-          component={() => <Link to={`/${user.userName}/grades`}>Grades</Link>}
-        />
-        <Route
-          path={`/instructor`}
-          component={() => (
-            <Link to={`/${user.userName}/students`}>Students</Link>
-          )}
-        />
-
-        {assignLinks}
+        <ul>
+          <li style={{ flexGrow: "1" }}>
+            <h1>
+              <span className="we">We</span>
+              <span className="can">Can</span>
+              <span className="grade">
+                {`{`}Grade{`}`}
+              </span>
+              <span className="it">IT</span>
+            </h1>
+          </li>
+          <li className="nav-assign">
+            <Route
+              path={`/${user.userName}`}
+              component={() => (
+                <div>
+                  <a href="#">Assignments</a>
+                  <ul
+                    className="assign-dropdown"
+                    style={{ position: "absolute", top: "10" }}
+                  >
+                    {assignLinks}
+                  </ul>
+                </div>
+              )}
+            />
+          </li>
+          <li>
+            <Route
+              path={`/${user.userName}`}
+              component={() => (
+                <Link to={`/${user.userName}/grades`}>Grades</Link>
+              )}
+            />
+          </li>
+          <li>
+            <Route
+              path={`/instructor`}
+              component={() => (
+                <Link to={`/${user.userName}/students`}>Students</Link>
+              )}
+            />
+          </li>
+        </ul>
       </nav>
     );
   }
