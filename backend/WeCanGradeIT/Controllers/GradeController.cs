c0d37defa4e59a32488fa2eb5baa5f250d4420d6
Alex Albright <alexanderjalbright@gmail.com>
2019-04-07 12:53:40 -0400

    refactored grade controller, moved some code to grade repo for create and edit

--- backend/WeCanGradeIT/Controllers/GradeController.cs
+++ backend/WeCanGradeIT/Controllers/GradeController.cs
@@ -13,45 +13,17 @@ namespace WeCanGradeIT.Controllers
     [ApiController]
     public class GradeController : ControllerBase
     {
-        IGradeRepository gradeRepo;
-        IStudentRepository studentRepo;
+        IGradeRepository repo;
 
-        public GradeController(IGradeRepository gradeRepo, IStudentRepository studentRepo)
+        public GradeController(IGradeRepository repo)
         {
-            this.gradeRepo = gradeRepo;
-            this.studentRepo = studentRepo;
+            this.repo = repo;
         }
 
         [HttpPost("{studentId}/{assignmentId}")]
         public ActionResult<bool> Post(int studentId, int assignmentId, [FromBody] string repoUrl)
         {
-            var student = studentRepo.GetById(studentId);
-            bool exists = false;
-            foreach(var grade in student.Grades)
-            {
-                if(grade.AssignmentId == assignmentId)
-                {
-                    exists = true;
-                }
-            }
-
-            if (exists)
-            {
-                var theGrade = student.Grades.Single(grade => grade.AssignmentId == assignmentId);
-                theGrade.RepoUrl = repoUrl;
-                gradeRepo.Edit(theGrade);
-            }
-            else
-            {
-                var newGrade = new Grade()
-                {
-                    StudentId = studentId,
-                    AssignmentId = assignmentId,
-                    RepoUrl = repoUrl
-                };
-                gradeRepo.Create(newGrade);
-            }
-
+            repo.CreateOrEdit(studentId, assignmentId, repoUrl);
             return true;
         }   
     }
---
+++ backend/WeCanGradeIT/Migrations/20190407155714_grades.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WeCanGradeIT.Migrations
+{
+    public partial class grades : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Grade_Assignments_AssignmentId",
+                table: "Grade");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Grade_Students_StudentId",
+                table: "Grade");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Grade",
+                table: "Grade");
+
+            migrationBuilder.RenameTable(
+                name: "Grade",
+                newName: "Grades");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Grade_StudentId",
+                table: "Grades",
+                newName: "IX_Grades_StudentId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Grade_AssignmentId",
+                table: "Grades",
+                newName: "IX_Grades_AssignmentId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Grades",
+                table: "Grades",
+                column: "GradeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Grades_Assignments_AssignmentId",
+                table: "Grades",
+                column: "AssignmentId",
+                principalTable: "Assignments",
+                principalColumn: "AssignmentId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Grades_Students_StudentId",
+                table: "Grades",
+                column: "StudentId",
+                principalTable: "Students",
+                principalColumn: "StudentId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Grades_Assignments_AssignmentId",
+                table: "Grades");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Grades_Students_StudentId",
+                table: "Grades");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Grades",
+                table: "Grades");
+
+            migrationBuilder.RenameTable(
+                name: "Grades",
+                newName: "Grade");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Grades_StudentId",
+                table: "Grade",
+                newName: "IX_Grade_StudentId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Grades_AssignmentId",
+                table: "Grade",
+                newName: "IX_Grade_AssignmentId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Grade",
+                table: "Grade",
+                column: "GradeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Grade_Assignments_AssignmentId",
+                table: "Grade",
+                column: "AssignmentId",
+                principalTable: "Assignments",
+                principalColumn: "AssignmentId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Grade_Students_StudentId",
+                table: "Grade",
+                column: "StudentId",
+                principalTable: "Students",
+                principalColumn: "StudentId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
