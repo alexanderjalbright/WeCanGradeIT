696cc27688dc63fb3d65162cddb99f01d48b41be
Joe Scheiman <scheiman.joe@gmail.com>
2019-04-16 01:07:29 -0400

    Realized I was trying to grab a string, instead of an OBJECT!

--- frontend/src/components/Student.js
+++ frontend/src/components/Student.js
@@ -6,21 +6,29 @@ export default class Student extends Component {
   constructor() {
     super();
     this.state = {
-      avatar_url: ""
+      info: {}
     };
   }
 
-  getImage() {
+  //   getImage() {
+  //     const { user } = this.props;
+  //     fetch(`${gitHubApi}/users/${user.userName}`)
+  //       .then(res => res.json())
+  //       .then(json => this.setState({ avatar_url: json }))
+  //       .then(console.log("howdy: " + this.state.avatar_url));
+  //   }
+
+  componentDidMount() {
     const { user } = this.props;
-    fetch(`${gitHubApi}/users/${user.userName}`)
+    fetch("https://api.github.com/users/alexanderjalbright")
       .then(res => res.json())
-      .then(json => this.setState({ avatar_url: json }))
-      .then(console.log("howdy: " + this.state.avatar_url));
+      .then(json => this.setState({ info: json }))
+      .then(console.log("howdy: " + this.state.info.avatar_url));
   }
 
   render() {
     const { user } = this.props;
-    this.getImage();
+    // this.getImage();
     return (
       <Route
         path={`/${user.userName}`}
@@ -31,7 +39,7 @@ export default class Student extends Component {
             <h1>{user.userName}</h1>
             {/* <h1>`${this.state.avatar_url}`</h1> */}
             <img
-              src={`${this.state.avatar_url}`}
+              src={`${this.state.info.avatar_url}`}
               style={{ width: "200px", margin: "1em 0" }}
             />
           </div>
